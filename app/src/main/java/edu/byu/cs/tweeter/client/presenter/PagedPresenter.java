3a54fd0aa49b541a9161a3d37d6c19016e495cf0
tom9493 <tom9493@gmail.com>
2022-03-06 19:59:43 -0700

    Added sample code shared files. App works

--- app/src/main/java/edu/byu/cs/tweeter/client/presenter/PagedPresenter.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/PagedPresenter.java
@@ -2,7 +2,6 @@ package edu.byu.cs.tweeter.client.presenter;
 
 import edu.byu.cs.tweeter.client.cache.Cache;
 import edu.byu.cs.tweeter.client.model.service.FollowService;
-import edu.byu.cs.tweeter.client.model.service.StatusService;
 import edu.byu.cs.tweeter.client.model.service.UserService;
 import edu.byu.cs.tweeter.client.model.service.observer.ServiceObserver;
 import edu.byu.cs.tweeter.model.domain.User;
